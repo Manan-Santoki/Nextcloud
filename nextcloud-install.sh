b065ab3332fedb02485f4a6707c51d5f3616ee7a
geek2gether <143657697+geek2gether@users.noreply.github.com>
2023-10-07 13:10:48 -0400

    Update nextcloud-install.sh

--- nextcloud-install.sh
+++ nextcloud-install.sh
@@ -12,12 +12,12 @@ if ! [ $( id -u ) = 0 ]; then
 fi
 
 
-# Colors to use for output
+# Output colors
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m' 
 
 # Log Location
 LOG="/tmp/nextcloud-install.log"
